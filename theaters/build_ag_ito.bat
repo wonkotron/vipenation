62bc9c66cae94e593ba0a104c47c6268cd627be4
wonkotron <wonkotron@gmail.com>
2025-08-01 14:20:16 -0700

    AG ITO:  copy test files source, launcher automatic detection

---
+++ theaters/build_ag_ito.bat
@@ -0,0 +1,2 @@
+set FALCON_BMS=D:\Games\Falcon BMS 4.38
+python ../build_theater.py -n "AirGoons ITO" -v 0.01 -c "build_ag_ito.json" -b "../build" -m "AirGoons ITO" -s "%FALCON_BMS%\Data\Add-On Israel"
