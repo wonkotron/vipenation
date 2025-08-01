82a7429b5957447721037103926253ab3fa455b4
wonkotron <wonkotron@gmail.com>
2025-08-01 16:10:46 -0700

    automatic detection of BMS install dir

--- build_theater.py
+++ build_theater.py
@@ -1,4 +1,21 @@
-import os, shutil, argparse, json
+import os, shutil, argparse, json, winreg
+
+
+def get_bms_dir():
+    bms_dir = None
+    key_path = r"SOFTWARE\WOW6432Node\Benchmark Sims\Falcon BMS 4.38"
+    value_name = "baseDir"
+    
+    registry = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+    key_handle = winreg.OpenKey(registry, key_path)
+    if not key_handle:
+        print("[ERROR] get_bms_dir():  could not open registry key")
+        return None
+    else:
+        bms_dir, _ = winreg.QueryValueEx(key_handle, value_name)
+
+    return bms_dir
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -26,6 +43,7 @@ class Configuration:
             self.subdirs = config_data.get("subdirs", None)
 
 
+
 def copy_from_source(source_path, build_dir, subdirs):  
     for dir in subdirs:
         subdir_path = "{0}/{1}".format(source_path, dir)
@@ -46,20 +64,25 @@ def build_artifacts(zip_path, build_path, package_name):
 
 def cleanup(build_dir):
     print("Removing working directories")
-    # shutil.rmtree(build_dir)
+    shutil.rmtree(build_dir)
 
 
 def main():
-    arguments = parse_arguments()
-    print(arguments.build)
+    bms_dir = get_bms_dir()
+    if bms_dir is None:
+        print("[ERROR] main:\tcould not query bms install directory")
+        return
 
+    arguments = parse_arguments()
     config = Configuration(arguments.config)
+
+    source_dir = "{0}/{1}".format(bms_dir, arguments.source)
     add_on_name = "Add-On {0}".format(arguments.name)
     build_dir = "{0}/{1}".format(arguments.build, add_on_name)
     zip_path = os.path.abspath("{0}/{1} v{2}".format(arguments.artifacts, arguments.name, arguments.version))
-    
+
     copy_from_mod(arguments.mod, build_dir)
-    copy_from_source(arguments.source, build_dir, config.subdirs)
+    copy_from_source(source_dir, build_dir, config.subdirs)
     build_artifacts(zip_path, arguments.build, add_on_name)
     cleanup(arguments.build)
     print("Done.")
--- theaters/build_ag_ito.bat
+++ theaters/build_ag_ito.bat
@@ -1,2 +1,4 @@
-set FALCON_BMS=D:\Games\Falcon BMS 4.38
-python ../build_theater.py -n "AirGoons ITO" -v 0.01 -c "build_ag_ito.json" -b "../build" -m "AirGoons ITO" -s "%FALCON_BMS%\Data\Add-On Israel"
+set NAME=AirGoons ITO
+set VER=0.01
+
+python ../build_theater.py -n "%NAME%" -v "%VER%" -c "build_ag_ito.json" -b "../build" -m "AirGoons ITO" -s "Data\Add-On Israel"
