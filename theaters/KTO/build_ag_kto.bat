ff2bcfb8d6ff00cb0d330481d488acaff35e0395
wonkotron <wonkotron@gmail.com>
2025-11-17 09:20:22 -0800

    fix build call

--- theaters/KTO/build_ag_kto.bat
+++ theaters/KTO/build_ag_kto.bat
@@ -1,4 +1,4 @@
 @ECHO off
 set NAME=AirGoons KTO
 set VER=0.01
-python ../../build_theater.py -n "%NAME%" -v "%VER%" -c "build_ag_kto.json" -b "../../build" -a "../../artifacts" -m "AirGoons KTO" -s "Data\"
+python ../../build_theater.py -n "%NAME%" -v "%VER%" -c "build_ag_kto.json" -b "../../build" -a "../../artifacts" -m "AirGoons KTO" -s "Data"
